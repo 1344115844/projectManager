8be6e3d4bf8e4f85fc192aca339f2585acca06d7
suveng <1344115844@qq.com>
2018-03-26 22:54:33 +0800

    ssm

---
+++ back/projectManager/src/main/java/cn/edu/hstc/attend/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.edu.hstc.attend.dao;
+
+import cn.edu.hstc.attend.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
