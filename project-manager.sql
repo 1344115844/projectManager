85b28d0bd43cb60bc33835f01edf25203b5bfec4
suveng <1344115844@qq.com>
2018-03-26 09:12:57 +0800

    create attend-conference and award and academic

--- project-manager.sql
+++ project-manager.sql
@@ -11,18 +11,80 @@
  Target Server Version : 100130
  File Encoding         : 65001
 
- Date: 25/03/2018 22:04:25
+ Date: 26/03/2018 09:10:49
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for academic
+-- ----------------------------
+DROP TABLE IF EXISTS `academic`;
+CREATE TABLE `academic`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学术论文',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
+  `deleteTime` datetime(0) NULL DEFAULT NULL,
+  `thesisName` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '论文名称\r\n',
+  `Novon` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '刊物名称\r\n',
+  `releasingCode` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发行代码\r\n',
+  `publicationLevel` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '刊物级别\r\n',
+  ` periodical` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '刊期\r\n',
+  `publishJournals` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发表刊物\r\n',
+  `collectionSituation` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收录情况\r\n',
+  `authorRank` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者排名\r\n',
+  `category` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类别\r\n',
+  `authorID` int(11) NULL DEFAULT NULL COMMENT '关联member表',
+  `fileID` int(11) NULL DEFAULT NULL COMMENT '关联filepath表',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for attend-conference
+-- ----------------------------
+DROP TABLE IF EXISTS `attend-conference`;
+CREATE TABLE `attend-conference`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '参加学术会议',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
+  `deleteTime` datetime(0) NULL DEFAULT NULL,
+  `conferenceName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '会议名称\r\n',
+  `paperTitle` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '论文题目',
+  `time` datetime(0) NULL DEFAULT NULL COMMENT '举办时间',
+  `place` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地点',
+  `hostUnit` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '主办单位',
+  `conferenceCategory` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '会议类别',
+  `attendID` int(11) NULL DEFAULT NULL COMMENT '参加人id，关联member表',
+  `fileID` int(11) NULL DEFAULT NULL COMMENT '附件id，关联filepath表',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for award
+-- ----------------------------
+DROP TABLE IF EXISTS `award`;
+CREATE TABLE `award`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '获奖信息',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
+  `deleteTime` datetime(0) NULL DEFAULT NULL,
+  `projectName` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目名称\r\n',
+  `awardName` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '获奖名称',
+  `awardLevel` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '获奖等级',
+  `awardTime` datetime(0) NULL DEFAULT NULL COMMENT '获奖时间',
+  `awardRank` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '获奖排行',
+  `awardID` int(11) NULL DEFAULT NULL COMMENT '获奖人id，关联member表',
+  `fileID` int(11) NULL DEFAULT NULL COMMENT '附件id，关联filepath表',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
 -- Table structure for filepath
 -- ----------------------------
 DROP TABLE IF EXISTS `filepath`;
 CREATE TABLE `filepath`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '附件表',
   `createTime` datetime(0) NULL DEFAULT NULL,
   `updateTime` datetime(0) NULL DEFAULT NULL,
   `deleteTime` datetime(0) NULL DEFAULT NULL,
@@ -35,8 +97,15 @@ CREATE TABLE `filepath`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '成员表',
+  `name` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '人员名称\r\n',
+  `jobTitle` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '职称\r\n',
+  `education` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学历',
+  `researchDirection` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '研究方向',
+  `AcademicTitle` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学术头衔\r\n',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
+  `deleteTime` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
@@ -45,7 +114,7 @@ CREATE TABLE `member`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project`  (
-  `id` int(25) NOT NULL AUTO_INCREMENT,
+  `id` int(25) NOT NULL AUTO_INCREMENT COMMENT '科研项目',
   `projectName` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT ' 项目名称\r\n',
   `projectNumber` int(10) NULL DEFAULT NULL COMMENT '编号\r\n',
   ` projectSource` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目来源',
@@ -58,6 +127,9 @@ CREATE TABLE `project`  (
   `projectDirector` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '课题负责人，关联member表\r\n',
   `fileID` int(20) NULL DEFAULT NULL COMMENT '附件地址关联filepath表',
   `memberID` int(10) NULL DEFAULT NULL COMMENT '参与人，关联member表',
+  `creatTime` datetime(0) NULL DEFAULT NULL,
+  `deleteTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
@@ -66,7 +138,12 @@ CREATE TABLE `project`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '账户表',
+  `username` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `daeleteTime` datetime(0) NULL DEFAULT NULL,
+  `updateTime` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
