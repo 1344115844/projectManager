bb2409dd202d54b3331604cebc46b5254f074e08
suveng <1344115844@qq.com>
2018-03-27 19:00:23 +0800

    test

--- back/projectManager/src/main/java/cn/edu/hstc/login/controller/LoginController.java
+++ back/projectManager/src/main/java/cn/edu/hstc/login/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,9 +33,11 @@ public class LoginController {
      * @return user[authority,username]
      */
     @RequestMapping("/check")
+    @ResponseBody
     public User check(@RequestParam String username,@RequestParam String password) {
         User user = new User();
-        userService.findUserByUsername();
+        user=userService.findUserByUsername();
+
 
         return user;
     }
