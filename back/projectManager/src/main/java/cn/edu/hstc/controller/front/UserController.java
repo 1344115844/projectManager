9edd681713dd8b79fa14dd52fcbb719f5fc2eb88
zyf <yiizhang66@gmail.com>
2018-03-29 13:10:36 +0800

    check login 方法有问题！

--- back/projectManager/src/main/java/cn/edu/hstc/controller/front/UserController.java
+++ back/projectManager/src/main/java/cn/edu/hstc/controller/front/UserController.java
@@ -3,20 +3,15 @@ package cn.edu.hstc.controller.front;
 import cn.edu.hstc.common.JSONResponse;
 import cn.edu.hstc.pojo.User;
 import cn.edu.hstc.service.UserService;
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-import org.apache.commons.logging.Log;
-import org.apache.shiro.SecurityUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
+
 
 /**
  * @author Veng Su 1344114844@qq.com
@@ -25,15 +20,11 @@ import java.security.NoSuchAlgorithmException;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    /*
-     *@author suveng
-     *@date 2018/3/27 20:30
-     *@param []
-     *@return java.lang.String
-     *方法作用：跳转index页面
-     **/
+
     @Autowired
     UserService userService;
+
+
 
     @RequestMapping("/index")
     public String show() {
