2a1b79b869e150c7e12450cf0f92a2d7f6e19b13
suveng <1344115844@qq.com>
2018-03-31 19:37:44 +0800

    新增user-list页面

--- back/projectManager/src/main/java/cn/edu/hstc/controller/back/AdminController.java
+++ back/projectManager/src/main/java/cn/edu/hstc/controller/back/AdminController.java
@@ -1,5 +1,7 @@
 package cn.edu.hstc.controller.back;
 
+import cn.edu.hstc.common.JSONResponse;
+import cn.edu.hstc.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("admin")
 public class AdminController {
-    @RequestMapping("/add/member")
-    public String showAdd(){
+
+
+    /**
+     *@author Veng Su
+     *@date 2018/3/31 19:32
+     *@param []
+     *@return java.lang.String
+     *方法作用：跳转到member-list页面
+     **/
+    @RequestMapping("/show/user-list")
+    public String showMember(){
         return "member-list";
     }
+
+    @RequestMapping("add/user")
+    public JSONResponse<User> addUser(){
+        return null;
+    }
+
+
 }
