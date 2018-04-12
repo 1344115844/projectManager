90a51cd6d33ed051836543fdf31b85ff0275ef67
suveng <1344115844@qq.com>
2018-04-12 08:39:05 +0800

    完成获取用户名

--- back/projectManager/src/main/webapp/myjs/until/username.js
+++ back/projectManager/src/main/webapp/myjs/until/username.js
@@ -1,5 +1,21 @@
 /**
  *@author Veng Su
+ *@date 2018/4/11 23:17
+ *方法作用：获取用户名并设置用户名
+ **/
+function getUsername() {
+    $.ajax({
+        type:"POST",
+        url:"/user/getUsername",
+        success:function (data) {
+            data=JSON.parse(data);
+
+            $("#username").html(data.data);
+        }
+    });
+}
+/**
+ *@author Veng Su
  *@date 2018/4/5 21:20
  *方法作用：url解析get参数
  **/
