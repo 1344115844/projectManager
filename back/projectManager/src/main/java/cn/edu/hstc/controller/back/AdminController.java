59bd1b685c2d6c0e42b991128f54d12f0eb5838f
suveng <1344115844@qq.com>
2018-05-07 16:11:05 +0800

    前端跳转

--- back/projectManager/src/main/java/cn/edu/hstc/controller/back/AdminController.java
+++ back/projectManager/src/main/java/cn/edu/hstc/controller/back/AdminController.java
@@ -6,9 +6,7 @@ import cn.edu.hstc.pojo.Role;
 import cn.edu.hstc.pojo.User;
 import cn.edu.hstc.service.RoleService;
 import cn.edu.hstc.service.UserService;
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Veng Su 1344114844@qq.com
@@ -44,6 +41,15 @@ public class AdminController extends BaseController{
         return "/admin/list";
     }
 
+    @RequestMapping("/permissions/list")
+    public String showPermissions(){
+        return "/admin/permissions_list";
+    }
+    @RequestMapping("/roles/list")
+    public String showRole(){
+        return "/admin/role_list";
+    }
+
 
 
     @RequestMapping("/selectAdmin")
@@ -64,7 +70,7 @@ public class AdminController extends BaseController{
      *@author Veng Su 2018/5/6 22:42
      *方法作用：查询所有角色和权限
      **/
-    @RequestMapping("/role")
+    @RequestMapping("/role/permission")
     @ResponseBody
     @RequiresRoles("administrator")
     public JSONResponse<ArrayList> selectAllRoleAndPermission(){
@@ -72,7 +78,7 @@ public class AdminController extends BaseController{
             ArrayList<Role> roles=roleService.selectAllRoleAndPermission();
             return JSONResponse.createBySuccess("success",roles);
         }catch (Exception e){
-            logger.error("admin/role接口出错");
+            logger.error("admin/role/permission接口出错");
             return JSONResponse.createByErrorMessage("内部错误");
         }
     }
